26f04856952e33c968697d58a1e904f729d3c1f4
Semenu Aggor <semenu@gmail.com>
2017-02-06 07:43:29 +0000

    Placing the server and acceptance test code within the same repo

--- model/elastic.go
+++
@@ -1,9 +0,0 @@
-package model
-
-import "gopkg.in/olivere/elastic.v3"
-
-type ElasticStatus struct {
-	Status       string              `json:"status"`
-	StatusCode   int                 `json:"statusCode,omitempty"`
-	PingResponse *elastic.PingResult `json:"pingResponse,omitempty"`
-}
--- model/website.go
+++
@@ -1,6 +0,0 @@
-package model
-
-type WebsiteStatus struct {
-	Status     string `json:"status"`
-	StatusCode int    `json:"statusCode,omitempty"`
-}
