fe330553672834b284d4a7ef63cfa77c4df760df
Semenu Aggor <semenu@gmail.com>
2017-02-10 09:56:40 +0000

    Adding a sad path search acceptance test

--- tests/java/src/test/java/uk/gov/ons/acceptance/metadata/SearchAcceptanceTest.java
+++ tests/java/src/test/java/uk/gov/ons/acceptance/metadata/SearchAcceptanceTest.java
@@ -12,6 +12,7 @@ import uk.gov.ons.api.ApiClient;
 import uk.gov.ons.api.model.Records;
 
 import static java.util.Collections.singletonList;
+import static org.apache.http.HttpStatus.SC_NOT_FOUND;
 import static org.apache.http.HttpStatus.SC_OK;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
@@ -32,20 +33,28 @@ public class SearchAcceptanceTest extends BaseAcceptanceTest {
             "<br>Then a page of records matching my search term should be returned" +
             "<hr>")
     public void shouldFindRecords() throws Exception {
-        final JsonObject expectedTimeseries = jsonReader
+        final JsonObject expectedRecords = jsonReader
                 .getJson(dataDir, "search.json")
                 .getAsJsonObject();
 
         final HttpResponse<Records> response = ApiClient.set().search("Travel Fare");
 
-        final JsonObject actualTimeseries = jsonParser.parse(gson.toJson(response.getBody())).getAsJsonObject();
+        final JsonObject actualRecords = jsonParser.parse(gson.toJson(response.getBody())).getAsJsonObject();
 
         assertThat(response.getStatus(), is(SC_OK));
         assertThat(response.getHeaders().get(CONTENT_TYPE), is(singletonList(APPLICATION_JSON)));
-        assertThat(actualTimeseries, is(expectedTimeseries));
+        assertThat(actualRecords, is(expectedRecords));
     }
 
+    @Test
+    @Title("Given that metadata index does not contains my search term" +
+            "<br>When I search" +
+            "<br>Then a response code of 404 should be returned" +
+            "<hr>")
+    public void shouldNotFindRecords() throws Exception {
+        final HttpResponse<Records> response = ApiClient.set().search("A dog chases a rose");
 
-    // don't find "a cat chases a dog"
+        assertThat(response.getStatus(), is(SC_NOT_FOUND));
+    }
 
 }
