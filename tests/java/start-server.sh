7ac7a5a95bbb720b918b4eb2d088851e510c4c16
Semenu Aggor <semenu@gmail.com>
2017-02-28 10:30:10 +0000

    Updating server scripts for use within concourse

--- tests/java/start-server.sh
+++ tests/java/start-server.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-docker run --name dp-api-elastic -p 9300:9300 -p 9200:9200 -e "http.host=0.0.0.0" -e "transport.host=127.0.0.1" -e ES_JAVA_OPTS="-Xms1g -Xmx1g" -d sem247/elasticns:2.4.3_1
-docker run --name dp-api-zebedee -p 8082:8082 -d sem247/zbdreader:1.0.0
-
-waitforit/wait-for-it.sh -t 0 -h localhost -p 9200
-waitforit/wait-for-it.sh -t 0 -h localhost -p 8082
+#docker run --name dp-api-elastic -p 9300:9300 -p 9200:9200 -e "http.host=0.0.0.0" -e "transport.host=127.0.0.1" -e ES_JAVA_OPTS="-Xms1g -Xmx1g" -d sem247/elasticns:2.4.3_1
+#docker run --name dp-api-zebedee -p 8082:8082 -d sem247/zbdreader:1.0.0
+#
+#waitforit/wait-for-it.sh -t 0 -h localhost -p 9200
+#waitforit/wait-for-it.sh -t 0 -h localhost -p 8082
 
 export API_APP_PORT=3000
 export ELASTICSEARCH_ROOT=http://localhost:9200
--- tests/java/stop-server.sh
+++ tests/java/stop-server.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 kill $(lsof -n -i :3000 | awk '/LISTEN/ {print $2}')
-
-docker stop dp-api-zebedee
-docker stop dp-api-elastic
-
-sleep 20
-
-docker rm dp-api-zebedee
-docker rm dp-api-elastic
+#
+#docker stop dp-api-zebedee
+#docker stop dp-api-elastic
+#
+#sleep 20
+#
+#docker rm dp-api-zebedee
+#docker rm dp-api-elastic
